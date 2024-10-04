407ec2f098ce7804bdb07a5b1690ae95671fde34
Justin Bottinga <0337445@student.rocvantwente.nl>
2024-10-04 15:24:48 +0200

    added text-field

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TextField from "./components/TextField/TextField";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TextField />
+    </>
+  );
 }
 
 export default App;
