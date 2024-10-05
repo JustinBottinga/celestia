9755203f903ed68f6bc780db7264222517b48781
Justin Bottinga <JBottinga@outlook.com>
2024-10-05 12:28:42 +0200

    background

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -29,41 +29,39 @@ function TextField(): JSX.Element {
   };
 
   return (
-    <>
-      <div className="container w- p-4 flex justify-center items-end flex-row h-screen box-border">
-        <form onSubmit={handleSubmit} method="post" autoComplete="off">
-          <div className="relative">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="rounded-full h-min bg-opacity-20 bg-purple-200 p-2 px-3 text-purple-100 outline-none focus:caret-transparent hover:cursor-pointer"
-            />
-            <button
-              className="absolute right-0 rounded-s-sm bg-opacity-40 bg-purple-100 p-2 px-3 rounded-full hover:bg-opacity-10 cursor-pointer transition-all"
-              role="submit"
+    <div className="w- p-4 flex justify-center items-end flex-row h-screen box-border">
+      <form onSubmit={handleSubmit} method="post" autoComplete="off">
+        <div className="relative">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="rounded-full h-min bg-opacity-20 bg-purple-200 p-2 px-3 text-purple-100 outline-none focus:caret-transparent hover:cursor-pointer"
+          />
+          <button
+            className="absolute right-0 rounded-s-sm bg-opacity-40 bg-purple-100 p-2 px-3 rounded-full hover:bg-opacity-10 cursor-pointer transition-all"
+            role="submit"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-purple-200"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6 text-purple-200"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                />
-              </svg>
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
