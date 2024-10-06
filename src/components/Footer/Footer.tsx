31e5dfbf33fb68101ec83f9f4f1e03de7a79b1d0
Justin Bottinga <JBottinga@outlook.com>
2024-10-06 17:12:37 +0200

    footer fontsize change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,39 @@
 import { Link } from "react-router-dom";
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <>
-            <footer className="border-t border-solid border-gray-100/5 text-white py-6 mt-10">
-                <div className="container mx-auto flex justify-between items-center p-5">
-                    <div>
-                        <h2 className="text-lg font-semibold">Celestia</h2>
-                        <p className="text-sm">Your AI friend to talk to!</p>
-                    </div>
-                    <div className="flex space-x-4">
-                        <Link to="/" className="text-white hover:text-purple-300">Home</Link>
-                        <Link to="/learn" className="text-white hover:text-purple-300">Learn more</Link>
-                        <Link to="/chat" className="text-white hover:text-purple-300">Chat</Link>
-                        <a href="#" className="text-white hover:text-purple-300">Log-in</a>
-                        <a href="#" className="text-white hover:text-purple-300">Register</a>
-                    </div>
-                </div>
-                <div className="text-center mt-4">
-                    <p className="text-sm">© 2024 Celestia. All rights reserved.</p>
-                </div>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer className="border-t border-solid border-gray-100/5 text-white py-6">
+        <div className="container mx-auto flex justify-between items-center p-5">
+          <div>
+            <h2 className="text-lg font-semibold font-serif">Celestia</h2>
+            <p className="text-sm">Your AI friend to talk to!</p>
+          </div>
+          <div className="flex gap-4 text-sm ">
+            <Link to="/" className="text-white hover:text-purple-300">
+              Home
+            </Link>
+            <Link to="/learn" className="text-white hover:text-purple-300">
+              Learn more
+            </Link>
+            <Link to="/chat" className="text-white hover:text-purple-300">
+              Chat
+            </Link>
+            <a href="#" className="text-white hover:text-purple-300">
+              Log-in
+            </a>
+            <a href="#" className="text-white hover:text-purple-300">
+              Register
+            </a>
+          </div>
+        </div>
+        <div className="text-center mt-4">
+          <p className="text-sm">© 2024 Celestia. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
